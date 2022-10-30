77aba789e70fce80db7558b4507e86e02d772047
111111 <hogwartsghls@gmail.com>
2022-10-31 07:52:03 +0800

    初始化

---
+++ src/main/java/com/liwa/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author asus
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2022-10-30 17:03:01
+* @Entity generator.domain.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/liwa/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.liwa.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.liwa.project.model.entity.User;
+
+/**
+ * @Entity com.liwa.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
